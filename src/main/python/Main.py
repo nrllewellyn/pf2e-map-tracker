d63b29d74386a26993077a76cd96fe59417f2b48
Nelson R. Llewellyn <4811061+nrllewellyn@users.noreply.github.com>
2026-01-28 00:59:10 -0500

    Switch to non-test data

--- src/main/python/Main.py
+++ src/main/python/Main.py
@@ -1,6 +1,6 @@
 from GraphExport import create_room_graph
 
 if __name__ == "__main__":
-    # json_path = "../resources/roomData.json"
-    json_path = "../resources/testData.json"
+    json_path = "../resources/roomData.json"
+    # json_path = "../resources/testData.json"
     create_room_graph(json_path, "../../../target/interactive_room_graph.html")
